77a0de5f2f4ddf63ce9f8f12c7407c91114104c9
Aniekan Afangideh <henryafangideh@gmail.com>
2024-03-22 13:16:01 +0100

    New print function

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ def addition(a,b,c):
 
 def mult(a,b):
     return a*b
-<<<<<<< HEAD
-=======
 
 def subtract(a,b):
     print(a-b)
-    return a-b
->>>>>>> 8e91cc5f6ad0185647647315df6b197293b15630
+    return a-b 
+    
+
